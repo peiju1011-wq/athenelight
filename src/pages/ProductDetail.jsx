ba32db0cdc957ee79458b31efac32652480b31d2
peiju1011-wq <peiju1011@gmail.com>
2026-04-22 15:19:11 +0800

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -82,7 +82,7 @@ export default function ProductDetail(){
 
   return(
 
-    <main className="pt-[120px] pb-32 bg-[#f7f7f7] min-h-screen">
+   <main className="pt-[120px] pb-32 bg-[#f7f7f7] min-h-screen overflow-x-hidden">
 
       {/* TOP */}
       <div className="max-w-[1100px] mx-auto px-6 mb-20">
