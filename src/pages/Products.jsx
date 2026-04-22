0067e36f8582400eea4ce68a070026a99e8088bb
peiju1011-wq <peiju1011@gmail.com>
2026-04-22 15:57:07 +0800

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -276,7 +276,7 @@ onClick={() => {
 
   ) : (
 
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
      {paginatedProducts.map(p=>{
 
