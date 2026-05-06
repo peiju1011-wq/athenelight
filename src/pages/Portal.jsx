17a4db18a0c1955ddcea9d1e1b0d49347f467052
peiju1011-wq <peiju1011@gmail.com>
2026-05-06 17:05:27 +0800

    Update Portal.jsx

--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -8,7 +8,7 @@ import { text } from "../data/text"
 import products from "../data/products";
 
 const slides = [
-  "/images/hero/hero4.jpg",
+  "/images/hero/hero4.png",
   "/images/hero/hero5.jpg",
   "/images/hero/hero6.jpg"
 ];
