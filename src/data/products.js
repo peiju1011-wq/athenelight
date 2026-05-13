a47ac206705bb1948321c3bf34bdb2e767ef4055
peiju1011-wq <peiju1011@gmail.com>
2026-05-13 15:59:29 +0800

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,32 +1,32 @@
 const products = [
   {
     id:1,
-    title:{ zh:"吊燈", en:"Chandelier" },
-    img:"/images/lights/pd1-1.png",
+    title:{ zh:"極簡吊燈", en:"Chandelier" },
+    img:"/images/lights/pd1-1-2.png",
     link:"linear-light"
   },
   {
     id:2,
-    title:{ zh:"雙環吊燈", en:"Double Ring Chandelier" },
-    img:"/images/lights/pd2-1.jpg",
+    title:{ zh:"鳥籠吊燈", en:"Birdcage Chandelier" },
+    img:"/images/lights/pd2-1-2.png",
     link:"double-ring-light"
   },
   {
     id:3,
-    title:{ zh:"石材壁燈", en:"Stone Wall Light"},
-    img:"/images/lights/pd3-1.png",
+    title:{ zh:"光紗吊燈", en:"Translucent Stone & Stretch Ceiling Integrated Lighting"},
+    img:"/images/lights/pd3-1-2.png",
     link:"stone-wall-light"
   },
   {
     id:4,
-    title:{ zh:"石材壁燈2", en:"Stone Wall Light2"},
-    img:"/images/lights/pd4-1.jpg",
+    title:{ zh:"星雨吊燈", en:"Starlight Rain Chandelier"},
+    img:"/images/lights/pd4-1-2.jpg",
     link:"stone-wall-light2"
   },
   {
     id:5,
-    title:{ zh:"石材床頭壁燈", en:"Stone Wall Bed Light" },
-    img:"/images/lights/pd5-10.jpg",
+    title:{ zh:"光境中島", en:"Lumen Island" },
+    img:"/images/lights/pd5-10-2.jpg",
     link:"stone-wall-light-bed"
   },
   {
