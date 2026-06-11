80de39235b49db5bce1a86ef91b810b19cfb964e
peiju1011-wq <peiju1011@gmail.com>
2026-06-11 14:09:41 +0800

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -36,9 +36,6 @@ return(
     {t?.contact?.title?.[lang]}
   </h1>
 
-  <p className="text-[#C8A46A] tracking-[0.3em] text-[24px] mt-12">
-    {lang === "en" ? "CONTACT ATHENE LIGHT" : "聯絡宇碩光"}
-  </p>
 
 </section>
 
