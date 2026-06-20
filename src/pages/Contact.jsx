11bdb6018cb40c341562d39dfdae32df3ae4a8bd
Panny <peiju1011@gmail.com>
2026-06-20 21:55:49 +0800

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -309,7 +309,7 @@ return(
 
       ${
         lang === "en"
-          ? "text-[10px] md:text-[12px] tracking-[0.25em] md:tracking-[0.25em]"
+          ? "text-[10px] md:text-[12px] tracking-[0.18em] md:tracking-[0.25em]"
           : "text-[13px] tracking-[0.3em]"
       }
     `}
